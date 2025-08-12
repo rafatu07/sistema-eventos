012f662f372382955ac1bf0fd7cfcf640159cc5a
Rafael Turino <rafa.tu@hotmail.com>
2025-08-12 14:32:26 -0300

    deploy 4

--- src/lib/text-utils.ts
+++ src/lib/text-utils.ts
@@ -9,7 +9,7 @@
 export const sanitizeTextForPDF = (text: string): string => {
   if (!text) return '';
   
-  let sanitized = text
+  const sanitized = text
     // Remover emojis
     .replace(/[\u{1F600}-\u{1F64F}]|[\u{1F300}-\u{1F5FF}]|[\u{1F680}-\u{1F6FF}]|[\u{1F1E0}-\u{1F1FF}]|[\u{2600}-\u{26FF}]|[\u{2700}-\u{27BF}]/gu, '')
     // Remover caracteres de controle
